ae114ec44c5eb1040af4854a406bea7314b881fc
suiman <suiman@Utopia.local>
2017-03-18 00:02:48 +0800

    add NotFoundExcetion

---
+++ Walker/Exception/NotFoundExcetion.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suiman
+ * Date: 2017/3/18
+ * Time: 上午12:00
+ */
+
+namespace Walker\Exception;
+
+
+class NotFoundExcetion extends WalkerException
+{
+
+}
